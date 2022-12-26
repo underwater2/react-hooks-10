e481d49a0b13c33fc19821e34c2cbaff582e4a13
underwater2 <shaian2@naver.com>
2022-12-26 15:16:29 +0900

    useTitle 작성

---
+++ useTitle/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useTitle } from "./useTitle";
+
+const App = () => {
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
+
+  return <div className="App"></div>;
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
